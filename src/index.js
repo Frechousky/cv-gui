5a81e784c828904c997f0f20c92a749eddc89e20
frechousky <alexandre.freche@gmail.com>
2023-01-03 18:50:02 +0100

    Load CV data from JS file instead of API call

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './main/components/App';
+import { cvData } from './main/data'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App data={cvData} />, document.getElementById('root'));
--- src/main/components/App.js
+++ src/main/components/App.js
@@ -7,7 +7,6 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 
 // App components
-import ApiLoadingError from './ApiLoadingError';
 import Header from './Header';
 import LoadingAnimation from './LoadingAnimation';
 
@@ -28,22 +27,8 @@ export default function App(props) {
     const [state, setState] = useState({
         selectedCv: 0,
         cvGuiLoading: false,
-        cvData: null,
-        cvDataLoading: true,
-        errorLoadingCvData: false,
-        refresh: 0
+        cvData: props.data,
     });
-    
-    useEffect(() => {
-        (async () => {
-            try {
-                var cvData = await getCv('http://localhost/cvs/1');
-                setState({...state, cvData: cvData, cvDataLoading: false});
-            } catch (err) {
-                setState({...state, errorLoadingCvData: true});
-            }
-        })();
-    }, [state.refresh]);
 
     const onClickCvLink = (i) => {
         if (state.selectedCv === i) {
@@ -51,53 +36,22 @@ export default function App(props) {
             return;
         }
         // set loading screen for several ms then display new CV gui
-        setState({...state, selectedCv: i, cvGuiLoading: true});
-        setTimeout(() => { setState({...state, selectedCv: i, cvGuiLoading: false}); }, 500);
+        setState({ ...state, selectedCv: i, cvGuiLoading: true });
+        setTimeout(() => { setState({ ...state, selectedCv: i, cvGuiLoading: false }); }, 500);
     };
 
-    const onRefreshClick = () => {
-        setState({...state, cvDataLoading: true, errorLoadingCvData: false, refresh: state.refresh + 1});
+    const generateSelectedCvHtml = () => {
+        return React.createElement(CVs[state.selectedCv].component, { data: state.cvData });
     };
 
-    const generateSelectedCvHtml = () => { return React.createElement(CVs[state.selectedCv].component, {data: state.cvData}); };
     return (
         <div className='row'>
             <div className='col-12'>
                 <Header cvs={CVs} selectedCv={state.selectedCv} onClickCvLink={onClickCvLink} disabled={state.cvGuiLoading || state.cvDataLoading} />
             </div>
-            <div className='col-12' style={{marginTop: '2rem'}}>
-                {state.errorLoadingCvData ? <ApiLoadingError onRefreshClick={onRefreshClick} /> : state.cvGuiLoading || state.cvDataLoading ? <LoadingAnimation /> : generateSelectedCvHtml()}
+            <div className='col-12' style={{ marginTop: '2rem' }}>
+                {state.cvGuiLoading ? <LoadingAnimation /> : generateSelectedCvHtml()}
             </div>
         </div>
     );
 };
-
-const getCv = async (cvUrl) => {
-    
-    const fetchResourceObject = async (fetchUrl) => {
-        return (await fetch(fetchUrl)).json();
-    }
-    
-    const fetchResourceArray = async (fetchUrl) => {
-        return (await fetchResourceObject(fetchUrl))._embedded;
-    }
-
-    const cv = await fetchResourceObject(cvUrl);
-    const _links = cv._links;
-
-    // Getting CVs related resources
-    cv.contactInformation = await fetchResourceObject(_links.contactInformation.href);
-    cv.languages = (await fetchResourceArray(_links.languages.href)).languages;
-    cv.projects = (await fetchResourceArray(_links.projects.href)).projects;
-    cv.skills = (await fetchResourceArray(_links.skills.href)).skills;
-    cv.studies = (await fetchResourceArray(_links.studies.href)).studies;
-    cv.workExperiences = (await fetchResourceArray(_links.workExperiences.href)).workExperiences;
-
-    // Get company for each work experience
-    // => Do not use cv.workExperiences.forEach as it is asynchronous
-    for (const workExperience of cv.workExperiences) {
-        workExperience.company = await fetchResourceObject(workExperience._links.company.href);
-    }
-    
-    return cv;
-}
