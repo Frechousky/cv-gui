efbb11768a8bd4469edf61ca41c1d109b9a58bd0
frechousky <alexandre.freche@gmail.com>
2023-03-27 19:35:32 +0200

    update skill display

--- src/cvs/b&w/components/SkillsBlock.js
+++ src/cvs/b&w/components/SkillsBlock.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {capitalize, trim} from 'underscore.string';
-
+import {capitalize} from 'underscore.string';
 import LeftPanelBlock from './LeftPanelBlock';
 
 export default class SkillsBlock extends LeftPanelBlock {
@@ -12,64 +10,13 @@ export default class SkillsBlock extends LeftPanelBlock {
 
   // overload
   renderInnerBlock() {
-    const skillList = this.props.skills.map((skill, i) => <SkillItem key={i} skill={skill} displayIcon={true} />);
+    const skills = this.props.skills
+        .map((skill) => capitalize(skill.label))
+        .join(', ');
     return (
       <div className="col-12">
-        {skillList}
+        {skills}
       </div>
     );
   };
-};
-
-export function SkillItem(props) {
-  let faIcon = props.displayIcon ? trim(props.skill.label).toLowerCase() : null;
-  faIcon = faIcon === 'javascript' ? 'js' : faIcon;
-  return (
-    <div className="row">
-      <div className="col-12">
-        {props.displayIcon ? <FontAwesomeIcon icon={['fab', faIcon]} size='2x' /> : capitalize(props.skill.label)}
-      </div>
-      <div className="col-12" style={{paddingBottom: '1rem'}}>
-        <SkillLevel level={props.skill.level} />
-      </div>
-    </div>
-  );
-};
-
-function SkillLevel(props) {
-  const skillStarsNb = 5;
-  const skillStars = [];
-  for (let i = 0; i < skillStarsNb; i++) {
-    const tmp = props.level - i;
-    if (tmp <= 0) {
-      skillStars.push(<SkillEmptyStar key={i} />);
-    } else if (tmp === 0.5) {
-      skillStars.push(<SkillHalfStar key={i} />);
-    } else if (tmp >= 1) {
-      skillStars.push(<SkillFullStar key={i} />);
-    }
-  }
-  return (
-    <div>
-      {skillStars}
-    </div>
-  );
-};
-
-function SkillFullStar(props) {
-  return (
-    <FontAwesomeIcon icon={['fas', 'star']} />
-  );
-};
-
-function SkillHalfStar(props) {
-  return (
-    <FontAwesomeIcon icon={['fas', 'star-half-alt']} />
-  );
-};
-
-function SkillEmptyStar(props) {
-  return (
-    <FontAwesomeIcon icon={['far', 'star']} />
-  );
 };
